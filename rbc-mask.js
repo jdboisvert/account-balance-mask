07cb3e59206609bdc20b04797271b9eabd85643a
jdboisvert <info.jeffreyboisvert@gmail.com>
2022-11-12 18:46:50 -0500

    Refactored the RBC masking to also mask the account options for the from and to accounts to not show the balance and only the account names.

--- rbc-mask.js
+++ rbc-mask.js
@@ -20,32 +20,62 @@ function maskAccounts() {
     }
 }
 
+function maskAccountSelectionOptions() {
+    const digitRegularExpression = /\d/;
+    const balanceAmountElements = document.querySelectorAll('option');
+    for (const balanceAmountElement of balanceAmountElements) {
+        const originalContent = balanceAmountElement.innerHTML;
+
+        if (!digitRegularExpression.test(originalContent)) {
+            // Ignore since value does not contain digits.
+            continue
+        }
+        
+        // We wish to simply show the account name not the balance. 
+        const currentInnerHTML = balanceAmountElement.innerHTML; 
+
+        // Assuming it is formatted as "Account Name = $123.12"
+        const currentInnerHTMLElements = currentInnerHTML.split(" = ");
+
+        if (currentInnerHTMLElements.length > 1) {
+            balanceAmountElement.innerHTML = currentInnerHTMLElements[0]; 
+        }
+    }
+}
+
 const target = document.querySelector('head > title');
 const observer = new window.WebKitMutationObserver(function(mutations) {
     mutations.forEach(function() {
-        maskAccounts();
+        const start = new Date().getTime();
+        const checkAccountBalancesInterval = setInterval(checkAccountBalances, 10);
+        const checkAccountOptionsInterval = setInterval(checkAccountOptions, 10);
+        const digitRegularExpression = /\d/;
+    
+        function checkAccountBalances() {
+            if (document.querySelectorAll('[class*="balance"]').length > 0) {
+                clearInterval(checkAccountBalancesInterval);
+                maskAccounts();
+            }
+    
+            if (new Date().getTime() - start > 15000) {
+                // Stop checking if it has not found anything after a set amount of time. 
+                clearInterval(checkAccountBalancesInterval);
+            }
+        }
+    
+        function checkAccountOptions() {
+            if (document.querySelectorAll('option').length > 0 && digitRegularExpression.test(document.querySelectorAll('option')[0].innerHTML)) {
+                clearInterval(checkAccountOptionsInterval);
+                maskAccountSelectionOptions();
+            }
+    
+            if (new Date().getTime() - start > 15000) {
+                // Stop checking if it has not found anything after a set amount of time. 
+                clearInterval(checkAccountOptionsInterval);
+            }
+        }
     });
 });
 observer.observe(target, { subtree: true, characterData: true, childList: true });
-
-function init() {
-    const start = new Date().getTime();
-    var checkInternal = setInterval(check, 100);
-
-    function check() {
-        if (document.querySelectorAll('[class*="balance"]').length > 0) {
-            clearInterval(checkInternal);
-            maskAccounts();
-        }
-
-        if (new Date().getTime() - start > 15000) {
-            // Stop checking if it has not found anything after a set amount of time. 
-            clearInterval(checkInternal);
-        }
-    }
-};
-
-window.addEventListener("load", init, false);
-
 
 
