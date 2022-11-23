6fb074f0019a1fc7ae866d9a4666703c40d5cdb5
jdboisvert <info.jeffreyboisvert@gmail.com>
2022-11-22 20:52:33 -0500

    Working solution where functions are more DRY.

--- rbc-mask.js
+++ rbc-mask.js
@@ -1,67 +1,62 @@
+const querySelectorStringBalance = '[class*="balance"]';
+const querySelectorStringDropdownOptions = 'option';
+
+const maxMillisecondsToSearch = 15000;
+const intervalAmount = 10;
+
 
 /*
  * Function used to mask the RBC balance details present on the page.
  */
 const maskAccounts = () => {
-    maskObjects('[class*="balance"]', maskObject);
-}
+    maskObjects(querySelectorStringBalance, maskObject);
+};
+
+/**
+ * Specific to the dropdown items that have account balances in them. 
+ * @param {*} htmlObject representing an object from the DOM
+ */
+const cleanDropdownAccountBalances = (htmlObject) => {
+    // We wish to simply show the account name not the balance. 
+    const currentInnerHTML = htmlObject.innerHTML; 
+
+    // Assuming it is formatted as "Account Name = $123.12"
+    const currentInnerHTMLElements = currentInnerHTML.split(" = ");
+    if (currentInnerHTMLElements.length > 1) {
+        htmlObject.innerHTML = currentInnerHTMLElements[0]; 
+    }
+};
 
 /*
  * Function used to remove the balance details in the account drop downs.
  */
-function maskAccountSelectionOptions() {
-    const digitRegularExpression = /\d/;
-    const balanceAmountElements = document.querySelectorAll('option');
-    for (const balanceAmountElement of balanceAmountElements) {
-        const originalContent = balanceAmountElement.innerHTML;
+const maskAccountSelectionOptions = () => {
+    maskObjects(querySelectorStringDropdownOptions, cleanDropdownAccountBalances);
 
-        if (!digitRegularExpression.test(originalContent)) {
-            // Ignore since value does not contain digits.
-            continue
-        }
-        
-        // We wish to simply show the account name not the balance. 
-        const currentInnerHTML = balanceAmountElement.innerHTML; 
-
-        // Assuming it is formatted as "Account Name = $123.12"
-        const currentInnerHTMLElements = currentInnerHTML.split(" = ");
-
-        if (currentInnerHTMLElements.length > 1) {
-            balanceAmountElement.innerHTML = currentInnerHTMLElements[0]; 
-        }
-    }
-}
+};
 
 const target = document.querySelector('head > title');
 const observer = new window.WebKitMutationObserver(function(mutations) {
+    /**
+     * Observing page to check when items to mask are present
+     */
     mutations.forEach(function() {
         const start = new Date().getTime();
-        const checkAccountBalancesInterval = setInterval(checkAccountBalances, 10);
-        const checkAccountOptionsInterval = setInterval(checkAccountOptions, 10);
-        const digitRegularExpression = /\d/;
+        const checkAccountBalancesInterval = setInterval(checkAccountBalances, intervalAmount);
+        const checkAccountOptionsInterval = setInterval(checkAccountOptions, intervalAmount);
     
         function checkAccountBalances() {
-            if (document.querySelectorAll('[class*="balance"]').length > 0) {
-                clearInterval(checkAccountBalancesInterval);
-                maskAccounts();
-            }
-    
-            if (new Date().getTime() - start > 15000) {
-                // Stop checking if it has not found anything after a set amount of time. 
-                clearInterval(checkAccountBalancesInterval);
-            }
+            const checkFunction = (querySelectorString) => {
+                return areLoaded(querySelectorString);
+            };
+            searchForObjectsToMask(querySelectorStringBalance, start, checkAccountBalancesInterval, maskAccounts, checkFunction, maxMillisecondsToSearch);
         }
-    
+
         function checkAccountOptions() {
-            if (document.querySelectorAll('option').length > 0 && digitRegularExpression.test(document.querySelectorAll('option')[0].innerHTML)) {
-                clearInterval(checkAccountOptionsInterval);
-                maskAccountSelectionOptions();
-            }
-    
-            if (new Date().getTime() - start > 15000) {
-                // Stop checking if it has not found anything after a set amount of time. 
-                clearInterval(checkAccountOptionsInterval);
-            }
+            const checkFunction = (querySelectorString) => {
+                return areLoaded(querySelectorString) && isFirstItemADigit(querySelectorString);
+            };
+            searchForObjectsToMask(querySelectorStringDropdownOptions, start, checkAccountOptionsInterval, maskAccountSelectionOptions, checkFunction, maxMillisecondsToSearch);
         }
     });
 });
--- utils.js
+++ utils.js
@@ -1,9 +1,10 @@
 const digitRegularExpression = /\d/;
 const maskingValue = "****";
 
-/*
+/**
  * Function used to mask the given HTML object
  * and assign it functions to show the original value on mouse over
+ * @param {*} htmlObject 
  */
 const maskObject = (htmlObject) => {
     const originalContent = htmlObject.innerHTML;
@@ -24,16 +25,58 @@ const maskObject = (htmlObject) => {
 };
 
 
-
-/*
+/**
  * Function used to mask several HTML objects that 
  * match the given query selector string (ex: '[class*="balance"]').
  * 
- * This takes as input a maskFunction which should take a htmlObject as input
+ * @param {*} querySelectorString match the given query selector string (ex: '[class*="balance"]').
+ * @param {*} maskFunction this takes as input a maskFunction which should take a htmlObject as input
  */
 const maskObjects = (querySelectorString, maskFunction) => {
     const htmlObjects = document.querySelectorAll(querySelectorString);
     for (const htmlObject of htmlObjects) {
         maskFunction(htmlObject);
     }
+};
+
+/**
+ * Function used to search for items on the page and will stop searching after a set amount of time.
+ * 
+ * @param {*} querySelectorString used to search for the items
+ * @param {*} startTime the timestamp of when the search started 
+ * @param {*} interval that is running the checks that needs to be cleared 
+ * @param {*} maskFunction handles the masking
+ * @param {*} checkFunction handles checking if the objects have been found (takes as input the querySelectorString)
+ * @param {*} maxMillisecondsToSearch an integer of milliseconds that should be spent searching
+ */
+ const searchForObjectsToMask = (querySelectorString, startTime, interval, maskFunction, checkFunction, maxMillisecondsToSearch) => {
+    if (checkFunction(querySelectorString)) {
+        clearInterval(interval);
+        maskFunction();
+    }
+
+    if (new Date().getTime() - startTime > maxMillisecondsToSearch) {
+        // Stop checking if it has not found anything after a set amount of time. 
+        clearInterval(interval);
+    }
+};
+
+/**
+ * Checks if any elements in the DOM are present matching the querySelectorString
+ * 
+ * @param {*} querySelectorString used to search for the items
+ * @returns true if first item is a digit false otherwise
+ */
+const areLoaded = (querySelectorString) => {
+    return document.querySelectorAll(querySelectorString).length > 0
+};
+
+/**
+ * Checks if the first item's html is a digit
+ * 
+ * @param {*} querySelectorString used to search for the items
+ * @returns true if first item is a digit false otherwise
+ */
+const isFirstItemADigit = (querySelectorString) => {
+    return digitRegularExpression.test(document.querySelectorAll(querySelectorString)[0].innerHTML);
 };
