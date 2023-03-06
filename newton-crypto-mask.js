8b82e082c607264cf274c4983bbf56d3d57e1881
Jeffrey <info.jeffreyboisvert@gmail.com>
2023-03-05 19:15:30 -0800

    Added support to mask the portfolio value and account values on the Newton Crypto dashboard. (#2)
    * Added support to mask the portfolio value and account values on the Newton Crypto dashboard.

---
+++ newton-crypto-mask.js
@@ -0,0 +1,50 @@
+const querySelectorPortfolioValue = '[id*="portfolio-value"]';
+const querySelectorStringAccountAmount = '[class*="sc-cXHkDB jLBty"]';
+
+
+const maxMillisecondsToSearch = 15000;
+const defaultInterval = 300;
+
+/*
+ * Function used to mask the total Portfolio Value present on the dashboard.
+ */
+const maskPortfolioValue = () => {
+    maskObjects(querySelectorPortfolioValue, maskObject);
+};
+
+/*
+* Function used to mask the account amounts present on the dashboard.
+*/
+const maskAccountAmounts = () => {
+    maskObjects(querySelectorStringAccountAmount, maskObject);
+};
+
+const target = document.querySelector('head > title');
+const observer = new window.WebKitMutationObserver(function(mutations) {
+    /**
+     * Observing page to check when items to mask are present
+     */
+    const start = new Date().getTime();
+    const portfolioValueInterval = setInterval(checkPortfolioValueId, defaultInterval);
+    const accountAmountClassInterval = setInterval(checkAccountAmountClasses, defaultInterval);
+    
+    function checkPortfolioValueId() {
+        const checkFunction = (querySelectorString) => {
+            return areLoaded(querySelectorString) && isFirstItemADigit(querySelectorString);
+        };
+        
+        searchForObjectsToMask(querySelectorPortfolioValue, start, portfolioValueInterval, maskPortfolioValue, checkFunction, maxMillisecondsToSearch);
+    }
+
+    function checkAccountAmountClasses() {
+        const checkFunction = (querySelectorString) => {
+            return areLoaded(querySelectorString) && isFirstItemADigit(querySelectorString);
+        };
+        
+        searchForObjectsToMask(querySelectorStringAccountAmount, start, accountAmountClassInterval, maskAccountAmounts, checkFunction, maxMillisecondsToSearch);
+    }
+});
+observer.observe(target, { subtree: true, characterData: true, childList: true });
+
+
+
